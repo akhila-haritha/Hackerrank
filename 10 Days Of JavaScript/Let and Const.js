27e3507eeeabfcc94e20bd11ce2f8650d6f35c5e
Akhila Angara <95868731+akhila-haritha@users.noreply.github.com>
2024-01-26 17:01:46 +0530

    Create Let and Const.js

---
+++ 10 Days Of JavaScript/Let and Const.js
@@ -0,0 +1,13 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    const r = readLine();
+    
+    // Print the area of the circle:
+    const area = PI*r*r;
+    const perimeter = 2*PI*r;
+    
+    // Print the perimeter of the circle:
+    console.log(area);
+    console.log(perimeter);
+}
