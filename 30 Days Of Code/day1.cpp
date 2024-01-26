a2e12fe7648f74f086ae7cb4d468cd0b6b61378f
Akhila Angara <95868731+akhila-haritha@users.noreply.github.com>
2024-01-26 16:50:03 +0530

    Create day1.cpp

---
+++ 30 Days Of Code/day1.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string ans;
+    cout<<"Hello, World."<<endl;
+    getline(cin,ans);
+    cout<<ans<<endl;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
